c0d1fa0c8cf3a81191e2ba7eb623f7e81fba68c3
Sy <phamcongtuansy@gmail.com>
2022-05-01 00:13:14 +0700

    version 1.0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,51 @@
+import axios from "axios";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useEffect, useState } from "react";
+import { isMobile } from "react-device-detect";
 import styles from "../styles/Home.module.css";
+import useFetch from "./hooks/useFetch";
+import { DATA_BUTTON, DATA_CONTENT } from "./store/data";
 
 const Home: NextPage = () => {
-  const data = [
-    "Khi bạn nhìn vào một thân cây hay một con người, từ sự tĩnh lặng ở trong bạn, thì ai đang nhìn vậy? Có một cái gì đó, sâu hơn là con người của bạn, đang nhìn. Đó là Tâm đang nhìn vào cái vật mà chính Tâm đã sáng tạo ra.",
-    `Nếu bạn tin rằng bạn chỉ là thân tâm này, bạn sẽ chết!
-  Nhưng khi bạn khám phá ra rằng bạn chính là Nhận Biết thì chuyện sinh tử chẳng bận lòng bạn nữa.`,
-    "Khi bên trong con có một cảm xúc tiêu cực nổ ra thì hãy thấu hiểu nó thay vì đánh nhau với nó. Tất cả các cảm xúc tiêu cực đều đến từ nỗi sợ một kết quả tiêu cực nào đó. Hãy cho cảm xúc đó xảy ra bất kể lý do gì, vì khi con cho nó xảy ra con mới thấy được bản chất của nó.",
-    "Chúng ta cần học cách nghỉ ngơi và thư giãn. Nó giúp ta phòng chống bệnh tật và giảm ngừa căng thẳng. Nó giúp ta có tâm trí sáng suốt để tập trung giải quyết các vấn đề",
-    `Nhớ rằng tôi sẽ chết, đó là công cụ quan trọng nhất tôi từng có để giúp mình thực hiện những lựa chọn lớn trong cuộc đời. Bởi vì hầu hết tất cả mọi thứ - tất cả những hy vọng phù phiếm, lòng kiêu hãnh, nỗi sợ mất mặt hay thất bại - tất cả những thứ này sẽ mờ nhạt đi khi ta đối diện với cái chết, chỉ để lại điều gì thật sự quan trọng.
-
-    Nhớ rằng bạn sẽ chết, đó là cách tốt nhất tôi biết để tránh cái bẫy suy nghĩ rằng bạn vẫn còn thứ để mất. Bạn thực chất chẳng có gì. Không có lí do để không đi theo trái tim.`,
-    "Khi bạn thật sự hiểu rằng bạn chỉ là một mảnh nhỏ của sự sống trong một vũ trụ bất tận, bạn sẽ tự nhiên cúi đầu trước mọi thứ.",
-    "Đừng lo cho ngày mai. Hãy để ngày mai mai lo. Hãy để lo lắng ngày nào đủ cho ngày ấy.",
-    "Nếu một việc gì đến mà ta không dũng cảm giải quyết cho tới nơi tới chốn, nó nhất định sẽ trở lại. Cuộc sống là vậy, nó sẽ lần lượt bắt mình thực hành mãi một bài học, cho đến khi thành thục mới thôi.",
-    "Chúng ta thường đánh giá thấp sức mạnh của nụ cười, của lời tử tế, của sự vỗ về, của tâm lắng nghe, của lời khen ngợi chân thành, hoặc một cử chỉ săn sóc rất nhỏ. Tất cả những điều ấy đều ẩn chứa khả năng thay đổi một cuộc đời.",
-    "Khôn ngoan chỉ đến qua con đường đau khổ. Có lẽ điều gì bắt trái tim ta tan nát chính là điều giúp trái tim rộng mở. Đau khổ giúp ta rất nhiều, nếu ta chấp nhận bài học mà đau khổ mang đến. Có lẽ chính thử thách lớn nhất trong đời sẽ ban tặng cơ hội lớn nhất.",
-    "Mỗi khi bạn phán xét ai đó là bạn tiết lộ một phần của bản thân cần được chữa lành.",
-  ];
-  const [text, setText] = useState(data[0]);
-  const [bg, setBg] = useState("https://picsum.photos/1600/900");
+  const [text, setText] = useState(DATA_CONTENT[0].text);
+  const [textButton, setTextButton] = useState(DATA_BUTTON[0].text);
+  const [bg, setBg] = useState("");
+  const [requesting, setRequesting] = useState(false);
 
   const randomText = () => {
     const numberRandom = Math.floor(Math.random() * 10) + 1;
-    setText(data[numberRandom]);
+    const numberButtonRandom = Math.floor(Math.random() * 2) + 1;
+
+    setText(DATA_CONTENT[numberRandom].text);
+    setTextButton(DATA_BUTTON[numberButtonRandom].text);
+  };
+
+  const changeBg = () => {
+    const numberFrom1To8 = Math.floor(Math.random() * 8) + 1;
+
+    if (isMobile) {
+      setBg(`https://www.picsum.photos/900/160${numberFrom1To8}`);
+    } else {
+      setBg(`https://www.picsum.photos/1600/90${numberFrom1To8}`);
+    }
+  };
+
+  const getUniverse = () => {
+    setRequesting(true);
+    randomText();
+    setTimeout(() => {
+      setRequesting(false);
+    }, 1000);
   };
 
   useEffect(() => {
-    setBg("https://picsum.photos/1600/900");
-  }, [text]);
+    if (isMobile) {
+      setBg("https://picsum.photos/900/1600");
+    } else {
+      setBg("https://picsum.photos/1600/900");
+    }
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -40,14 +55,27 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className={styles.base} style={{ background: `url(${bg})` }}>
+      <div
+        className={styles.base}
+        style={{
+          background: `url(${bg})`,
+        }}
+      >
         <div className={styles.content}>
           <div className={styles.text}>{text}</div>
           <div className={styles.btnContainer}>
-            <div className={styles.buttonText} onClick={() => randomText()}>
-              Xin quẻ nè
+            <div className={styles.buttonText} onClick={() => getUniverse()}>
+              {textButton}
             </div>
           </div>
+        </div>
+        <div className={styles.changBG}>
+          <div
+            className={styles.buttonRandomAnimation}
+            onClick={() => changeBg()}
+          />
+          <div className={styles.buttonChangeBG} onClick={() => changeBg()} />
+          <div className={styles.buttonIDontKnow} onClick={() => changeBg()} />
         </div>
       </div>
     </div>
